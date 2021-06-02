2690a7ffe8ad4fe0352d23e651c0efa5349888b2
Anna Nawrocka <anna.nawrocka1988@gmail.com>
2021-06-02 08:42:55 +0100

    Add marker options

--- script.js
+++ script.js
@@ -28,10 +28,26 @@ if (navigator.geolocation)
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-        .openPopup();
+      map.on("click", function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+
+        const options = { riseOnHover: true };
+
+        L.marker([lat, lng], options)
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: "running-popup",
+            })
+          )
+          .setPopupContent("Workout")
+          .openPopup();
+      });
     },
     function () {
       alert("Could not get your position");
